1563c07250b6bfb0aeb5c47dae9d8caabfc29990
Nikhil chopra <nikhil0080012005@gmail.com>
2024-06-28 13:59:22 +0530

    middleware in next

---
+++ middleware-test/middleware.ts
@@ -0,0 +1,12 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export function middleware(request : NextRequest){
+    console.log(request.nextUrl.pathname)
+    if(request.nextUrl.pathname.startsWith('/admin')){
+        return NextResponse.redirect(new URL('/signin' , request.url))
+    }
+
+    if(request.nextUrl.pathname.startsWith('/dashboard')){
+        return NextResponse.next()
+    }
+}
